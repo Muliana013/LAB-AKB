34bbbfc1b4ff170f261c4011576bad06798621d6
Muliana013 <118157271+Muliana013@users.noreply.github.com>
2025-07-17 15:11:13 +0800

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -9,53 +9,58 @@ import {
   StyleSheet,
 } from 'react-native';
 
-const MAIN_IMAGES = [
-  'https://images.pexels.com/photos/1546166/pexels-photo-1546166.jpeg',
-  'https://images.pexels.com/photos/1396132/pexels-photo-1396132.jpeg',
-  'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg',
-  'https://images.pexels.com/photos/672916/pexels-photo-672916.jpeg',
-  'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg',
-  'https://images.pexels.com/photos/2847648/pexels-photo-2847648.jpeg',
-  'https://images.pexels.com/photos/280222/pexels-photo-280222.jpeg',
-  'https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg',
-  'https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg',
+// Gambar Hello Kitty (gunakan 9 gambar untuk grid 3x3)
+const HELLO_KITTY_IMAGES = [
+  'https://i.pinimg.com/736x/1a/24/97/1a24973a70046e304372c7e41aaca367.jpg',
+  'https://i.pinimg.com/originals/5a/b1/93/5ab193449a1806a4a0ad199a4c812d06.jpg',
+  'https://i.pinimg.com/originals/e5/e4/9a/e5e49a7332c2c9beebf2490205c97cb5.jpg',
+  'https://i.pinimg.com/originals/f4/2c/88/f42c880826274e0bd501f2a12e3cf3f1.jpg',
+  'https://i.pinimg.com/originals/38/ea/4e/38ea4e1e7a6aeb4f122eaa3d2f456e27.jpg',
+  'https://i.pinimg.com/originals/f7/02/55/f7025525dd2806d002b40c1f281206c4.jpg',
+  'https://i.pinimg.com/originals/c8/71/bf/c871bf47956b208f7026b73187c55709.jpg',
+  'https://i.pinimg.com/originals/5a/ab/3f/5aab3f83ef2d9f8b59821b1f6d9d6e59.jpg',
+  'https://i.pinimg.com/originals/90/dc/55/90dc556416e7fd22e8d876e3e5d4d3de.jpg',
 ];
 
+// Gambar alternatif untuk flip
 const ALT_IMAGES = [
-  'https://images.pexels.com/photos/221540/pexels-photo-221540.jpeg',
-  'https://images.pexels.com/photos/415687/pexels-photo-415687.jpeg',
-  'https://images.pexels.com/photos/164522/pexels-photo-164522.jpeg',
-  'https://images.pexels.com/photos/25284/pexels-photo-25284.jpg',
-  'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg',
-  'https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg',
-  'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg',
-  'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg',
-  'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg',
+  'https://i.pinimg.com/originals/2e/d7/87/2ed787da981511bbce6a3c204627c490.jpg',
+  'https://i.pinimg.com/originals/ea/18/93/ea189340a6b3b86e08d3dfaff6b4ae60.jpg',
+  'https://i.pinimg.com/originals/4e/e0/fb/4ee0fb5a28c2b36ef49a4f8f208c1c3f.jpg',
+  'https://i.pinimg.com/originals/1a/b3/34/1ab3342adccdfec52a2f28204fa71c7e.jpg',
+  'https://i.pinimg.com/originals/3a/80/37/3a8037d0f1c3b5e1ad67f3c1d7b5311e.jpg',
+  'https://i.pinimg.com/originals/bb/32/88/bb3288e9a2dbcc729988b847938b1cd4.jpg',
+  'https://i.pinimg.com/originals/f9/8f/67/f98f67c1938933482a08027b69aa3a8a.jpg',
+  'https://i.pinimg.com/originals/f3/77/7b/f3777b3e8936d27b02a80261cd6b2b8a.jpg',
+  'https://i.pinimg.com/originals/84/03/91/84039136b2962f2693efc26c36b400e5.jpg',
 ];
 
-const IMAGE_LIST = MAIN_IMAGES.map((main, index) => ({
+// Gabungkan jadi objek
+const IMAGE_LIST = HELLO_KITTY_IMAGES.map((main, index) => ({
   id: index,
   main,
   alt: ALT_IMAGES[index],
 }));
 
-const GridImage = ({ item }) => {
+const GridImage = ({ item, size }) => {
   const [useAlt, setUseAlt] = useState(false);
   const [scale, setScale] = useState(1);
 
   const handlePress = () => {
     setUseAlt(!useAlt);
-    setScale((prev) => {
-      const next = prev * 1.2;
-      return next <= 2 ? next : 2;
-    });
+    setScale((prev) => (prev * 1.2 <= 2 ? prev * 1.2 : 2));
   };
 
   return (
-    <TouchableOpacity onPress={handlePress} style={styles.imageWrapper}>
+    <TouchableOpacity onPress={handlePress} style={{ width: size, height: size }}>
       <Image
         source={{ uri: useAlt ? item.alt : item.main }}
-        style={[styles.image, { transform: [{ scale }] }]}
+        style={{
+          width: '100%',
+          height: '100%',
+          transform: [{ scale }],
+          borderRadius: 10,
+        }}
         resizeMode="cover"
       />
     </TouchableOpacity>
@@ -72,7 +77,7 @@ export default function App() {
         <View style={styles.grid}>
           {IMAGE_LIST.map((item) => (
             <View key={item.id} style={[styles.gridItem, { width: itemSize, height: itemSize }]}>
-              <GridImage item={item} />
+              <GridImage item={item} size={itemSize} />
             </View>
           ))}
         </View>
@@ -100,13 +105,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#222',
     borderRadius: 10,
     overflow: 'hidden',
-  },
-  imageWrapper: {
-    flex: 1,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
   },
 });
